505ee6c3387183d716d755211c506373589f7f04
Coding4Toon <coding4toon@gmail.com>
2023-07-07 18:31:22 -0500

    Add quotes data

---
+++ src/quote.js
@@ -0,0 +1,102 @@
+export const quote = [
+    {
+        id: 1,
+        content: '"Success is not final, failure is not fatal: It is the courage to continue that counts."',
+        author: 'Winston Churchill',
+    },
+    {
+        id: 2,
+        content: '"The future belongs to those who believe in the beauty of their dreams."',
+        author: 'Eleanor Roosevelt',
+    },
+    {
+        id: 3,
+        content: '"All our dreams can come true if we have the courage to pursue them."',
+        author: 'Walt Disney',
+    },
+    {
+        id: 4,
+        content: '"Believe you can and you\'re halfway there."',
+        author: 'Theodore Roosevelt',
+    },
+    {
+        id: 5,
+        content: '"The only way to do great work is to love what you do."',
+        author: 'Steve Jobs',
+    },
+    {
+        id: 6,
+        content: '"You miss 100% of the shots you don\'t take."',
+        author: 'Wayne Gretzky',
+    },
+    {
+        id: 7,
+        content: '"The best revenge is massive success."',
+        author: 'Frank Sinatra',
+    },
+    {
+        id: 8,
+        content: '"The only limit to our realization of tomorrow will be our doubts of today."',
+        author: 'Franklin D. Roosevelt',
+    },
+    {
+        id: 9,
+        content: '"The harder I work, the luckier I get."',
+        author: 'Thomas Jefferson',
+    },
+    {
+        id: 10,
+        content: '"Success usually comes to those who are too busy to be looking for it."',
+        author: 'Henry David Thoreau',
+    },
+    {
+        id: 11,
+        content: '"Don\'t be pushed around by the fears in your mind. Be led by the dreams in your heart."',
+        author: 'Roy T. Bennett',
+    },
+    {
+        id: 12,
+        content: '"The biggest risk is not taking any risk. In a world that\'s changing quickly, the only strategy that is guaranteed to fail is not taking risks."',
+        author: 'Mark Zuckerberg',
+    },
+    {
+        id: 13,
+        content: '"Your time is limited, don\'t waste it living someone else\'s life."',
+        author: 'Steve Jobs',
+    },
+    {
+        id: 14,
+        content: '"Success is not the key to happiness. Happiness is the key to success. If you love what you are doing, you will be successful."',
+        author: 'Albert Schweitzer',
+    },
+    {
+        id: 15,
+        content: '"It does not matter how slowly you go, as long as you do not stop."',
+        author: 'Confucius',
+    },
+    {
+        id: 16,
+        content: '"The only person you are destined to become is the person you decide to be."',
+        author: 'Ralph Waldo Emerson',
+    },
+    {
+        id: 17,
+        content: '"Success is walking from failure to failure with no loss of enthusiasm."',
+        author: 'Winston Churchill',
+    },
+    {
+        id: 18,
+        content: '"Believe in yourself and all that you are. Know that there is something inside you that is greater than any obstacle."',
+        author: 'Christian D. Larson',
+    },
+    {
+        id: 19,
+        content: '"Dream big and dare to fail."',
+        author: 'Norman Vaughan',
+    },
+    {
+        id: 20,
+        content: '"Dreams are the touchstones of our character."',
+        author: 'Henry David Thoreau',
+    },
+]
